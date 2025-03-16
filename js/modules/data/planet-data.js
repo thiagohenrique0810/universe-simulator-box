ef79b2c0540f520160b0f07c2b2ebb75d2ccdc5c
Thiago Henrique <thiagohenrique0810@hotmail.com>
2025-03-15 23:58:26 -0300

    ajustando as orbitas

--- js/modules/data/planet-data.js
+++ js/modules/data/planet-data.js
@@ -14,8 +14,8 @@ export const PLANET_DATA = {
     mercurio: {
         radius: 0.14,
         textureUrl: 'textures/mercury.jpg',
-        distance: 13,
-        semiMajorAxis: 13,
+        distance: 20,
+        semiMajorAxis: 20,
         eccentricity: 0.205,
         orbitalSpeed: 0.016,
         rotationSpeed: 0.004,
@@ -25,8 +25,8 @@ export const PLANET_DATA = {
     venus: {
         radius: 0.34,
         textureUrl: 'textures/venus.jpg',
-        distance: 17,
-        semiMajorAxis: 17,
+        distance: 30,
+        semiMajorAxis: 30,
         eccentricity: 0.007,
         orbitalSpeed: 0.0065,
         rotationSpeed: 0.002,
@@ -36,8 +36,8 @@ export const PLANET_DATA = {
     terra: {
         radius: 0.36,
         textureUrl: 'textures/earth.jpg',
-        distance: 20,
-        semiMajorAxis: 20,
+        distance: 40,
+        semiMajorAxis: 40,
         eccentricity: 0.017,
         orbitalSpeed: 0.004,
         rotationSpeed: 0.02,
@@ -46,9 +46,9 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'lua',
-                radius: 0.1,
+                radius: 0.098,
                 textureUrl: 'textures/moon.jpg',
-                distance: 1.0,
+                distance: 1.2,
                 orbitalSpeed: 0.03,
                 rotationSpeed: 0.01,
                 eccentricity: 0.0549
@@ -58,8 +58,8 @@ export const PLANET_DATA = {
     marte: {
         radius: 0.19,
         textureUrl: 'textures/mars.jpg',
-        distance: 25,
-        semiMajorAxis: 25,
+        distance: 55,
+        semiMajorAxis: 55,
         eccentricity: 0.094,
         orbitalSpeed: 0.0021,
         rotationSpeed: 0.018,
@@ -68,18 +68,18 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'fobos',
-                radius: 0.02,
+                radius: 0.004,
                 color: 0xaaaaaa,
-                distance: 0.5,
+                distance: 0.4,
                 orbitalSpeed: 0.04,
                 rotationSpeed: 0.01,
                 eccentricity: 0.015
             },
             {
                 name: 'deimos',
-                radius: 0.01,
+                radius: 0.002,
                 color: 0x888888,
-                distance: 0.7,
+                distance: 0.6,
                 orbitalSpeed: 0.02,
                 rotationSpeed: 0.008,
                 eccentricity: 0.0005
@@ -89,8 +89,8 @@ export const PLANET_DATA = {
     jupiter: {
         radius: 1.2,
         textureUrl: 'textures/jupiter.jpg',
-        distance: 46,
-        semiMajorAxis: 46,
+        distance: 100,
+        semiMajorAxis: 100,
         eccentricity: 0.049,
         orbitalSpeed: 0.0003,
         rotationSpeed: 0.04,
@@ -99,47 +99,56 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'io',
-                radius: 0.08,
+                radius: 0.033,
                 color: 0xdbb963,
-                distance: 1.8,
+                distance: 1.5,
                 orbitalSpeed: 0.015,
                 rotationSpeed: 0.005,
                 eccentricity: 0.0041
             },
             {
                 name: 'europa',
-                radius: 0.07,
+                radius: 0.028,
                 color: 0xb5a7a0,
-                distance: 2.2,
+                distance: 2.4,
                 orbitalSpeed: 0.01,
                 rotationSpeed: 0.005,
                 eccentricity: 0.0094
             },
             {
                 name: 'ganimedes',
-                radius: 0.12,
+                radius: 0.042,
                 color: 0x8e8373,
-                distance: 2.7,
+                distance: 3.8,
                 orbitalSpeed: 0.006,
                 rotationSpeed: 0.003,
                 eccentricity: 0.0013
             },
             {
                 name: 'calisto',
-                radius: 0.11,
+                radius: 0.038,
                 color: 0x3a3a3a,
-                distance: 3.3,
+                distance: 6.7,
                 orbitalSpeed: 0.004,
                 rotationSpeed: 0.002,
                 eccentricity: 0.0074
+            },
+            {
+                name: 'amalteia',
+                radius: 0.006,
+                color: 0xaa5533,
+                distance: 1.1,
+                orbitalSpeed: 0.02,
+                rotationSpeed: 0.006,
+                eccentricity: 0.003
             }
         ]
     },
     saturno: {
         radius: 0.96,
         textureUrl: 'textures/saturn.jpg',
-        distance: 62,
-        semiMajorAxis: 62,
+        distance: 150,
+        semiMajorAxis: 150,
         eccentricity: 0.057,
         orbitalSpeed: 0.0001,
         rotationSpeed: 0.01,
@@ -149,29 +158,74 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'titan',
-                radius: 0.14,
+                radius: 0.04,
                 color: 0xf5c466,
-                distance: 2.5,
+                distance: 5.0,
                 orbitalSpeed: 0.008,
                 rotationSpeed: 0.003,
                 eccentricity: 0.0288
             },
             {
                 name: 'encélado',
-                radius: 0.04,
+                radius: 0.008,
                 color: 0xe8e8e8,
-                distance: 2.0,
+                distance: 1.6,
                 orbitalSpeed: 0.01,
                 rotationSpeed: 0.004,
                 eccentricity: 0.0047
+            },
+            {
+                name: 'mimas',
+                radius: 0.004,
+                color: 0xdddddd,
+                distance: 1.2,
+                orbitalSpeed: 0.012,
+                rotationSpeed: 0.005,
+                eccentricity: 0.0196
+            },
+            {
+                name: 'tétis',
+                radius: 0.01,
+                color: 0xcccccc,
+                distance: 2.0,
+                orbitalSpeed: 0.009,
+                rotationSpeed: 0.004,
+                eccentricity: 0.0001
+            },
+            {
+                name: 'dione',
+                radius: 0.01,
+                color: 0xdddddd,
+                distance: 2.5,
+                orbitalSpeed: 0.007,
+                rotationSpeed: 0.003,
+                eccentricity: 0.0022
+            },
+            {
+                name: 'reia',
+                radius: 0.014,
+                color: 0xd8d8d8,
+                distance: 3.5,
+                orbitalSpeed: 0.006,
+                rotationSpeed: 0.003,
+                eccentricity: 0.0012
+            },
+            {
+                name: 'jápeto',
+                radius: 0.014,
+                color: 0x8a8a8a,
+                distance: 9.0,
+                orbitalSpeed: 0.004,
+                rotationSpeed: 0.002,
+                eccentricity: 0.0283
             }
         ]
     },
     urano: {
         radius: 0.62,
         textureUrl: 'textures/uranus.jpg',
-        distance: 88,
-        semiMajorAxis: 88,
+        distance: 200,
+        semiMajorAxis: 200,
         eccentricity: 0.046,
         orbitalSpeed: 0.00004,
         rotationSpeed: 0.03,
@@ -180,29 +234,56 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'titania',
-                radius: 0.05,
+                radius: 0.012,
                 color: 0x999999,
-                distance: 1.5,
+                distance: 2.7,
                 orbitalSpeed: 0.007,
                 rotationSpeed: 0.003,
                 eccentricity: 0.0011
             },
             {
                 name: 'oberon',
-                radius: 0.04,
+                radius: 0.012,
                 color: 0x777777,
-                distance: 1.8,
+                distance: 3.6,
                 orbitalSpeed: 0.005,
                 rotationSpeed: 0.002,
                 eccentricity: 0.0014
+            },
+            {
+                name: 'ariel',
+                radius: 0.009,
+                color: 0xbbbbbb,
+                distance: 1.9,
+                orbitalSpeed: 0.008,
+                rotationSpeed: 0.004,
+                eccentricity: 0.0012
+            },
+            {
+                name: 'umbriel',
+                radius: 0.009,
+                color: 0x555555,
+                distance: 2.3,
+                orbitalSpeed: 0.007,
+                rotationSpeed: 0.003,
+                eccentricity: 0.0039
+            },
+            {
+                name: 'miranda',
+                radius: 0.005,
+                color: 0xaaaaaa,
+                distance: 1.3,
+                orbitalSpeed: 0.01,
+                rotationSpeed: 0.005,
+                eccentricity: 0.0013
             }
         ]
     },
     netuno: {
         radius: 0.60,
         textureUrl: 'textures/neptune.jpg',
-        distance: 110,
-        semiMajorAxis: 110,
+        distance: 250,
+        semiMajorAxis: 250,
         eccentricity: 0.011,
         orbitalSpeed: 0.00002,
         rotationSpeed: 0.032,
@@ -211,12 +292,39 @@ export const PLANET_DATA = {
         satellites: [
             {
                 name: 'tritao',
-                radius: 0.08,
+                radius: 0.021,
                 color: 0xd1ccc1,
-                distance: 1.8,
+                distance: 2.2,
                 orbitalSpeed: 0.01,
                 rotationSpeed: 0.004,
                 eccentricity: 0.000016
+            },
+            {
+                name: 'proteu',
+                radius: 0.004,
+                color: 0x999999,
+                distance: 1.1,
+                orbitalSpeed: 0.012,
+                rotationSpeed: 0.005,
+                eccentricity: 0.0005
+            },
+            {
+                name: 'nereida',
+                radius: 0.003,
+                color: 0xaaaaaa,
+                distance: 14.0,
+                orbitalSpeed: 0.006,
+                rotationSpeed: 0.003,
+                eccentricity: 0.7512
+            },
+            {
+                name: 'larissa',
+                radius: 0.002,
+                color: 0x888888,
+                distance: 0.8,
+                orbitalSpeed: 0.015,
+                rotationSpeed: 0.006,
+                eccentricity: 0.0014
             }
         ]
     }
